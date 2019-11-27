7d0efe62b8b01620ee702a982aedbc18a7f2597b
yasintz <yasinntazeoglu@gmail.com>
2019-11-27 20:55:31 +0300

    added slice helper

--- src/server/utils/json-parser.ts
+++ src/server/utils/json-parser.ts
@@ -9,4 +9,16 @@ registerHelper('filter', ({ args, node, next }) => {
   return next(node);
 });
 
+registerHelper('slice', ({ node, args, next }) => {
+  try {
+    if (Array.isArray(node)) {
+      return next(node.slice(parseInt(args[0], 10), parseInt(args[1], 10)));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return next(node);
+});
+
 export default jsonParser;
