e7873544bab684facf7293d74d7f749fd1b72858
yasintz <yasinntazeoglu@gmail.com>
2019-12-14 02:35:56 +0300

    updated links

--- src/server/utils/link.ts
+++ src/server/utils/link.ts
@@ -1,16 +1,13 @@
 import { Request } from 'express';
 
-const httpsProtocol = process.env.NODE_ENV === 'development' ? 'http://' : 'https://';
 function generateApiLink(req: Request, id?: string, isPublic = false) {
-  const originUrl = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`).origin;
-  const apiUrl = originUrl.replace('http://', httpsProtocol);
+  const apiUrl = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`).origin;
   const url = new URL(`${isPublic ? '' : '/api'}/json/${id || ''}`, apiUrl).href;
 
   return url;
 }
 function generateViewLink(req: Request, id?: string) {
-  const originUrl = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`).origin;
-  const apiUrl = originUrl.replace('http://', httpsProtocol);
+  const apiUrl = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`).origin;
   const url = new URL(id ? `?id=${id}` : '', apiUrl).href;
 
   return url;
